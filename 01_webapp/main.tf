cea0d87de8d1ff5ad54efb57fe857d1fb6d204be
yunqian44 <yunqian8@live.com>
2020-06-05 17:42:41 +0800

    keep terraform state in clound

--- 01_webapp/main.tf
+++ 01_webapp/main.tf
@@ -1,21 +1,21 @@
-# terraform {
-#   required_version = ">= 0.12"
-#   backend "azurerm" {
-#     storage_account_name = "${var.storageaccount_name}"
-#     container_name       = "${var.container_name}"
-#     key                  = "${var.container_key}"
-#     access_key           = "${var.access_key}"
-#     features {}
-#   }
-# }
+terraform {
+  required_version = ">= 0.12"
+  backend "azurerm" {
+    storage_account_name = "${var.storageaccount_name}"
+    container_name       = "${var.container_name}"
+    key                  = "${var.container_key}"
+    access_key           = "${var.access_key}"
+    features {}
+  }
+}
 provider "azurerm" {
   version = "~>2.0.0"
   features {}
 
-  subscription_id = "9fb11b51-9734-459f-9262-611921729aed"
-  client_id       = "d6ae9674-892d-408c-ae51-24f0e4e68cad"
-  client_secret   = "33d7a90f-2ad7-4493-8cc0-232f50d0c8a9"
-  tenant_id       = "79461cf6-3db1-48ff-8018-6b87bd3d5b35"
+#   subscription_id = "9fb11b51-9734-459f-9262-611921729aed"
+#   client_id       = "d6ae9674-892d-408c-ae51-24f0e4e68cad"
+#   client_secret   = "33d7a90f-2ad7-4493-8cc0-232f50d0c8a9"
+#   tenant_id       = "79461cf6-3db1-48ff-8018-6b87bd3d5b35"
 }
 resource "azurerm_resource_group" "dev" {
   name     = "${var.resource_group_name}"
