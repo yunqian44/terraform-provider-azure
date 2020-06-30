8fdae700677986c2e67f2867ac6396d96a9ec660
yunqian44 <yunqian8@live.com>
2020-06-30 14:56:49 +0800

    暂时提交

---
+++ 03_aks/examples/simple/test/main.tf
@@ -0,0 +1,8 @@
+  
+module "tf-ref-aks-module" {
+  source             = "../../"
+  environment        = "${var.environment}"
+  location           = "${var.location}"
+  kubernetes_version = "${var.kubernetes_version}"
+  ssh_public_key     = "${file(var.ssh_public_key_file)}"
+}
